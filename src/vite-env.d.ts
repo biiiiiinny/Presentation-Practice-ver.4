9bf178ab3862aad9d03bd1f3efe9aac6a307bc57
biiiiiinny <seongbin0621@koreatech.ac.kr>
2026-02-26 16:00:34 +0900

    Node.js의 기본 타입 정의 설치

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module "*.png" {
-  const value: string;
-  return value;
-}
-
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.svg";
